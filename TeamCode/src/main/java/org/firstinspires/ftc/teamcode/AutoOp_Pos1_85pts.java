f08a2be8043ad1a67524de318ddb184324d7dcbb
11905ThePack <32345853+11905ThePack@users.noreply.github.com>
2017-12-02 12:49:16 -0800

    It made me commit..

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp_Pos1_85pts.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp_Pos1_85pts.java
@@ -72,12 +72,12 @@ public class AutoOp_Pos1_85pts extends LinearOpMode {
     HardwarePushbot         robot   = new HardwarePushbot();   // Use a Pushbot's hardware
     private ElapsedTime     runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
+    static  double     COUNTS_PER_MOTOR_REV    = 1440 ;    // eg: TETRIX Motor Encoder
     static final double     DRIVE_GEAR_REDUCTION    = 2.0 ;     // This is < 1.0 if geared UP
     static final double     WHEEL_DIAMETER_INCHES   = 4.0 ;     // For figuring circumference
     static final double     COUNTS_PER_INCH         = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                                       (WHEEL_DIAMETER_INCHES * 3.1415);
-    static final double     DRIVE_SPEED             = 0.6;
+    static final finaldouble     DRIVE_SPEED             = 0.6;
     static final double     TURN_SPEED              = 0.5;
 
     @Override
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test_Autonomous.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.eventloop.o
+
+@Autonomous(name="Test_Autonomous", group ="Position0")
+
+public class Test_Autonomous {
+
+@Override
+public void init() {
+
+
+
+    }
+
+}
