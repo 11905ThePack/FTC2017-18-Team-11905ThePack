d4411b013932c82ac42f8743786990e01dd1e1a2
11905ThePack <32345853+11905ThePack@users.noreply.github.com>
2018-02-04 16:38:50 -0800

    Worked on Jewel Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp_Jewel.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoOp_Jewel.java
@@ -32,6 +32,7 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.GyroSensor;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.TouchSensor;
 import com.qualcomm.robotcore.util.ElapsedTime;
@@ -55,23 +56,27 @@ public class AutoOp_Jewel extends LinearOpMode {
     ColorSensor JewelWhackerColorSensor;
 
     ElapsedTime eTime = new ElapsedTime();
-
     ElapsedTime eTime2 = new ElapsedTime();
+    ElapsedTime totaleTime = new ElapsedTime();
 
     DcMotor DriveLeftFront;
     DcMotor DriveLeftRear;
-
     DcMotor DriveRightFront;
     DcMotor DriveRightRear;
 
+    DcMotor MotorGlyphGrabber;
 
     private Servo JewelWhackerServo; //Jewel Whacker Servo
+    private double servoJewelWhackerServoPosition = 0;
+
     private Servo GlyphServoLeft; //Left half of glyph grabber
+    private double servoGlyphLeftPosition = 180;
+    private Servo GlyphServoRight; //Right half of glyph grabber
+    private double servoGlyphRightPosition = 0;
 
     TouchSensor TeamBlueSwitch;
+    GyroSensor Gyro;
 
-    private double servoJewelWhackerServoPosition = 0;
-    private double servoGlyphLeftPosition = 180;
 
     @Override
     public void runOpMode() {
@@ -83,12 +88,19 @@ public class AutoOp_Jewel extends LinearOpMode {
         final float values[] = hsvValues;
 
         JewelWhackerColorSensor = hardwareMap.get(ColorSensor.class, "JewelWhackerColorSensor");
+        JewelWhackerServo = hardwareMap.get(Servo.class,"JewelWhackerServo");
+
+        MotorGlyphGrabber = hardwareMap.get(DcMotor.class, "MotorGlyphGrabber");
+        MotorGlyphGrabber.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        MotorGlyphGrabber.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        GlyphServoLeft = hardwareMap.get(Servo.class, "GlyphServoLeft");
+        GlyphServoRight = hardwareMap.get(Servo.class, "GlyphServoRight");
+
 
         TeamBlueSwitch = hardwareMap.get(TouchSensor.class,"TeamBlueSwitch");
         boolean TeamBlueSwitchRead = TeamBlueSwitch.isPressed();
 
-
-        JewelWhackerServo = hardwareMap.get(Servo.class,"JewelWhackerServo");
+        Gyro = hardwareMap.get(GyroSensor.class, "Gyro");
 
         DriveLeftFront = hardwareMap.get(DcMotor.class,"DriveLeftFront");
         DriveLeftFront.setDirection(DcMotor.Direction.REVERSE);
@@ -103,80 +115,152 @@ public class AutoOp_Jewel extends LinearOpMode {
         servoJewelWhackerServoPosition = 10;
         JewelWhackerServo.setPosition(servoJewelWhackerServoPosition / 180); //This converts from degrees into 0-1 automagically.
 
+        GlyphServoLeft.setPosition(servoGlyphLeftPosition / 180);
+        GlyphServoRight.setPosition(servoGlyphRightPosition / 180);
+
+        Gyro.calibrate();
+        telemetry.addData("Glyph GrabberPos", MotorGlyphGrabber.getCurrentPosition());
+        telemetry.update();
 
         // wait for the start button to be pressed.
         waitForStart();
+        totaleTime.reset();
+
+        servoGlyphLeftPosition = 45;
+        GlyphServoLeft.setPosition(servoGlyphLeftPosition / 180);
+        servoGlyphRightPosition = 135;
+        GlyphServoRight.setPosition(servoGlyphRightPosition / 180);
+        sleep(500); //Wait for Servos
+
+        while (MotorGlyphGrabber.getCurrentPosition() < 500){
+            MotorGlyphGrabber.setPower(.25);
+            telemetry.addData("Glyph GrabberPos", MotorGlyphGrabber.getCurrentPosition());
+            telemetry.update();
+        }
+            MotorGlyphGrabber.setPower(0);
 
         servoJewelWhackerServoPosition = 95;
+        JewelWhackerServo.setPosition(servoJewelWhackerServoPosition / 180); //This converts from degrees into 0-1 automagically
+
+        eTime2.reset();
+        while (eTime2.time() < 1) {}
+
+        servoJewelWhackerServoPosition = 117;
         JewelWhackerServo.setPosition(servoJewelWhackerServoPosition / 180); //This converts from degrees into 0-1 automagically.
 
+        boolean GlyphNotRead = true;
         while (opModeIsActive()) {
+            while (GlyphNotRead) {
 
-            TeamBlueSwitchRead = TeamBlueSwitch.isPressed();
-            //If this reads true, red team.
-            //If this reads false, blue team.
+                TeamBlueSwitchRead = TeamBlueSwitch.isPressed();
+
+                //Rotate Towards Read Jewel
+                if (((JewelWhackerColorSensor.red() >= 2) && (!TeamBlueSwitchRead)) || ((JewelWhackerColorSensor.blue() >= 2) && (TeamBlueSwitchRead))) {
+
+                    telemetry.addData("ConsoleOut", "Attack!");
+
+                    GlyphNotRead = false;
+
+                    while (Gyro.getHeading() != 345){
+                        DriveLeftFront.setPower(-0.1);
+                        DriveLeftRear.setPower(-0.1);
+                        DriveRightFront.setPower(0.1);
+                        DriveRightRear.setPower(0.1);}
+
+                    DriveLeftFront.setPower(0);
+                    DriveLeftRear.setPower(0);
+                    DriveRightFront.setPower(0);
+                    DriveRightRear.setPower(0);
+
+                    servoJewelWhackerServoPosition = 10;
+                    JewelWhackerServo.setPosition(servoJewelWhackerServoPosition / 180);
+
+                    eTime.reset();
+                    while (eTime.time() < .5) {}
+
+                    while (Gyro.getHeading() != 0){
+                        DriveLeftFront.setPower(0.1);
+                        DriveLeftRear.setPower(0.1);
+                        DriveRightFront.setPower(-0.1);
+                        DriveRightRear.setPower(-0.1);}
+
+                    DriveLeftFront.setPower(0);
+                    DriveLeftRear.setPower(0);
+                    DriveRightFront.setPower(0);
+                    DriveRightRear.setPower(0);
 
 
-            eTime2.reset();
-            while (eTime2.time() < 1) {}
+                } else if (((JewelWhackerColorSensor.blue() >= 2) && (!TeamBlueSwitchRead)) || ((JewelWhackerColorSensor.red() >= 2) && (TeamBlueSwitchRead))) {
 
-            servoJewelWhackerServoPosition = 117;
-            JewelWhackerServo.setPosition(servoJewelWhackerServoPosition / 180); //This converts from degrees into 0-1 automagically.
+                    telemetry.addData("ConsoleOut", "Retreat!");
 
-            // send the info back to driver station using telemetry function.
-            telemetry.addData("Clear", JewelWhackerColorSensor.alpha());
-            telemetry.addData("Red  ", JewelWhackerColorSensor.red());
-            telemetry.addData("Green", JewelWhackerColorSensor.green());
-            telemetry.addData("Blue ", JewelWhackerColorSensor.blue());
-            telemetry.addData("TeamBlueSwitch", TeamBlueSwitchRead);
+                    GlyphNotRead = false;
 
-            //Rotate Towards Read Jewel
-            if (((JewelWhackerColorSensor.red() >= 2) && (TeamBlueSwitchRead)) || ((JewelWhackerColorSensor.blue() >= 2) && (!TeamBlueSwitchRead))) {
+                    while (Gyro.getHeading() != 15){
+                    DriveLeftFront.setPower(0.1);
+                    DriveLeftRear.setPower(0.1);
+                    DriveRightFront.setPower(-0.1);
+                    DriveRightRear.setPower(-0.1);}
 
-                telemetry.addData("ConsoleOut", "Rotate Away!");
+                    DriveLeftFront.setPower(0);
+                    DriveLeftRear.setPower(0);
+                    DriveRightFront.setPower(0);
+                    DriveRightRear.setPower(0);
 
-                DriveLeftFront.setPower(-0.1);
-                DriveLeftRear.setPower(-0.1);
-                DriveRightFront.setPower(0.1);
-                DriveRightRear.setPower(0.1);
+                    servoJewelWhackerServoPosition = 10;
+                    JewelWhackerServo.setPosition(servoJewelWhackerServoPosition / 180);
 
-                eTime.reset();
-                while (eTime.time() < 1) {
+                    eTime.reset();
+                    while (eTime.time() < .5) {}
+
+                    while (Gyro.getHeading() != 0){
+                        DriveLeftFront.setPower(-0.1);
+                        DriveLeftRear.setPower(-0.1);
+                        DriveRightFront.setPower(0.1);
+                        DriveRightRear.setPower(0.1);}
+
+                    DriveLeftFront.setPower(0);
+                    DriveLeftRear.setPower(0);
+                    DriveRightFront.setPower(0);
+                    DriveRightRear.setPower(0);
                 }
 
-                DriveLeftFront.setPower(0);
-                DriveLeftRear.setPower(0);
-                DriveRightFront.setPower(0);
-                DriveRightRear.setPower(0);
+                // send the info back to driver station using telemetry function.
+                telemetry.addData("Clear", JewelWhackerColorSensor.alpha());
+                telemetry.addData("Red", JewelWhackerColorSensor.red());
+                telemetry.addData("Green", JewelWhackerColorSensor.green());
+                telemetry.addData("Blue", JewelWhackerColorSensor.blue());
+                telemetry.addData("GyroPos", Gyro.getHeading());
 
+                if (TeamBlueSwitchRead) {
+                    telemetry.addData("TeamBlueSwitch", "Switch ON");
+                } else {
+                    telemetry.addData("TeamBlueSwitch", "Switch OFF");
+                }
+
+                telemetry.update();
 
             }
-            if (((JewelWhackerColorSensor.blue() >= 2) && (TeamBlueSwitchRead)) || ((JewelWhackerColorSensor.red() >= 2) && (!TeamBlueSwitchRead))) {
-
-                telemetry.addData("ConsoleOut", "Attack!!!!");
-
-                DriveLeftFront.setPower(0.1);
-                DriveLeftRear.setPower(0.1);
-                DriveRightFront.setPower(-0.1);
-                DriveRightRear.setPower(-0.1);
-
-                eTime.reset();
-                while (eTime.time() > 1) {}
-
-                DriveLeftFront.setPower(0);
-                DriveLeftRear.setPower(0);
-                DriveRightFront.setPower(0);
-                DriveRightRear.setPower(0);
-            }
-
-            TeamBlueSwitchRead = TeamBlueSwitch.isPressed();
-            telemetry.addData("ConsoleOut", "Not Read/Other Color");
+            JewelWhackerColorSensor.enableLed(false);
+            telemetry.addData("ConsoleOut", "Job's Done");
             telemetry.update();
 
-        }
-        JewelWhackerColorSensor.enableLed(false);
-        telemetry.addData("ConsoleOut", "Job's Done");
-        telemetry.update();
-    }
+            //Next Phase of Autonomous
 
+
+
+
+
+
+
+
+
+
+
+            while (totaleTime.time() < 30) {
+            telemetry.addData("ConsoleOut", "Finished, Wait for end.");
+            telemetry.update();
+            }
+        }
+    }
 }
